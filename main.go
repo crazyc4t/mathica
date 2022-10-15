b50c291970d9fd7896666adf79e3b99f0a2a712d
Said Neder <snederc@protonmail.com>
2022-10-15 17:42:26 -0500

    First commit

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	token  string
+	config *Config
+)
+
+type Config struct {
+	Token string `json:"token"`
+}
+
+func readConfig() {
+	file, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		log.Fatal("Error when opening file:", err)
+	}
+
+	fmt.Println("Reading configuration successful")
+
+	err = json.Unmarshal(file, &config)
+	if err != nil {
+		log.Fatal("Error when converting JSON", err)
+	}
+
+	token = config.Token
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "+help" {
+		s.ChannelMessageSend(m.ChannelID, "This is a work in progress, there's too much to be done")
+	}
+}
+
+func main() {
+	readConfig()
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
